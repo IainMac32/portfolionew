7873ecd360ff337e9928cab2495b911676f67cd0
IainMac32 <iainhmacdonald@gmail.com>
2025-05-21 20:23:28 -0400

    new cursor

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,20 @@
-import React from 'react';
 import '../styles/HomePage.css';
+import React, { useEffect } from 'react';
 
 function HomePage() {
-  return (
-    <div className="homepage-container">
-      {/* You can add any content here if you want */}
-    </div>
-  );
+  useEffect(() => {
+    const handleMouseEnter = () => {
+      document.body.style.cursor = 'url(p.png), auto';
+    };
+
+    window.addEventListener('mouseenter', handleMouseEnter);
+
+    return () => {
+      window.removeEventListener('mouseenter', handleMouseEnter);
+    };
+  }, []);
+
+  return <div className="homepage-container"></div>;
 }
 
 export default HomePage;
